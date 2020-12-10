64dd2f02be4b51ce7916a7cfc2b34352cf371cca
Joseph <joscschinn@gmail.com>
2020-12-10 17:03:02 -0600

    Defined Event time and repeat days logic

--- src/com/smartcalendar/Event.java
+++ src/com/smartcalendar/Event.java
@@ -1,5 +1,71 @@
 package com.smartcalendar;
 
+import java.util.Date;
+
 public class Event {
+
+	// INT TYPES
+	//
+
+	//Days of Week Types
+	static final byte SUNDAY = 0b01000000;
+	static final byte MONDAY = 0b00100000;
+	static final byte TUESDAY = 0b00010000;
+	static final byte WEDNESDAY = 0b00001000;
+	static final byte THURSDAY = 0b00000100;
+	static final byte FRIDAY = 0b00000010;
+	static final byte SATURDAY = 0b00000001;
+	
+	//PRIORITY SCALE
+	static final int PRIORITY_CONSTANT=10000000;
+	static final int PRIORITY_HIGH=10000;
+	static final int PRIORITY_MEDIUM=1000;
+	static final int PRIORITY_LOW=100;
+	static final int PRIORITY_DYNAMIC=0;
+	
+	
+	
+	Date start;
+	Date end;
+	int priority;
+	String name;
+	String description;
+	int type;
+	byte days = 0;
+
+	
+	/**
+	 * 
+	 * @param start
+	 * @param duration    duration of the event in milliseconds
+	 * @param priority
+	 * @param name
+	 * @param description
+	 * @param type
+	 */
+	public Event(Date start, int duration, int priority, String name, String description, int type) {
+		this.start = start;
+		this.end = new Date(start.getTime() + duration * 60000);
+		this.priority = priority;
+		this.name = name;
+		this.description = description;
+		this.type = type;
+	}
+
+	public void addRepeatDay(byte day) {
+		days |= day;
+	}
+
+	public void removeRepeatDay(byte day) {
+		days &= ~day;
+	}
+
+	public boolean isRepeatDay(byte day) {
+
+		return (days & day) != 0;
+
+	}
+
+
 
 }
--- src/com/smartcalendar/Main.java
+++ src/com/smartcalendar/Main.java
@@ -1,8 +1,25 @@
 package com.smartcalendar;
 
+import java.time.Instant;
+import java.util.Date;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		UIManager.openNew();
+		
+		
+		Event event = new Event(new Date(),15,0,"Test","Hello",0);
+		event.addRepeatDay(Event.SUNDAY);
+		event.addRepeatDay(Event.WEDNESDAY);
+		event.addRepeatDay(Event.SATURDAY);
+		event.removeRepeatDay(Event.FRIDAY);
+		
+		System.out.printf(Integer.toBinaryString(event.days));
+		
+		
+		
+		
+		
 	}
 }
